c35ac8fed6d5bf6e79ec125292e7a23a5ae28794
Stephen Martin <stephen.martin@cerner.com>
2018-05-10 22:24:19 -0500

    Uhh... can't remember what I added.

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
-const express = require('express');
+const app = require('express')();
 const graphHTTP = require('express-graphql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const schema = require('./schemas');
 const routes = require('./routes');
-const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
-
-// cloudinary.uploader.upload(
-//   image,
-//   (result) => {
-//     console.log(result);
-//   }, 
-//   {
-//     crop: 'fill',
-//     width: 36,
-//     height: 36,
-//     quality: 50,
-//     gravity: 'face',
-//     tags: ['avatar'],
-//   }
-// );
 
 app.use('/api', graphHTTP({
   schema,
